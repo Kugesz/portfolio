1137f30ffbc58ca2c421b0f5725b9a8bf64e7626
Kugesz <kovacsgergo.algyo@gmail.com>
2025-03-27 23:51:13 +0100

    deleted router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import Navigation from "./components/Navigation";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 
 const root = ReactDOM.createRoot(
@@ -11,13 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     <Navigation />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/projects" element={<div>Projektek</div>} />
-        <Route path="/contact" element={<div>Kapcsolat</div>} />
-      </Routes>
-    </BrowserRouter>
+    <Main />
   </>
 );
 
