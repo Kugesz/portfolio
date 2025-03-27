6505fc16f1b1f49144730c59b1e304c6d1040490
GergoKovacs0 <c9kovger@vasvari.org>
2025-03-27 23:40:22 +0100

    Add translations for navigation and project descriptions in English and Hungarian

---
+++ src/content/Translations.ts
@@ -0,0 +1,69 @@
+export const translationsNavigation = {
+  gb: {
+    brand: "Gergő Kovács",
+    projects: "Projects",
+    contact: "Contact",
+    portfolio: "Portfolio",
+    fileName: "portfolio_en.pdf",
+  },
+  hu: {
+    brand: "Kovács Gergő",
+    projects: "Projectek",
+    contact: "Kapcsolat",
+    portfolio: "Portfólió",
+    fileName: "portfolio_hu.pdf",
+  },
+};
+
+export const translations = {
+  gb: {
+    introduction: {
+      name: "Gergő Kovács",
+      title: "Junior Software Developer",
+      description: "",
+      interests: "",
+    },
+    projects: {
+      title: "Projects",
+      list: [
+        {
+          title: "adri.py",
+          status: "In progress",
+          description:
+            "I created adri.py back in high school, which was a Python automation script that downloaded class schedule changes from our school's website and was able to notify students on various platforms.",
+        },
+        {
+          title: "sequlizer to diagrams",
+          status: "In progress",
+          description:
+            "This project can generate database diagrams from Sequelize tables.",
+        },
+      ],
+    },
+  },
+  hu: {
+    introduction: {
+      name: "Kovács Gergő",
+      title: "Junior Softvare Fejlesztő",
+      description: "",
+      interests: "",
+    },
+    projects: {
+      title: "Projektek",
+      list: [
+        {
+          title: "adri.py",
+          status: "In progress",
+          description:
+            "Az adri.py-t még középiskolában készítettem ami egy python automatizéció volt, amely az iskolánk weboldaláról letöltötte az óracseréket és különböző platformokon képes volt diákokat értesíteni.",
+        },
+        {
+          title: "adri.py",
+          status: "sequlizer to diagrams",
+          description:
+            "Ez a projekt sequlizer táblákból képes adatbázis diagrammokat készíteni.",
+        },
+      ],
+    },
+  },
+};
