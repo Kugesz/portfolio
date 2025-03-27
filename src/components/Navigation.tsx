3c5ef157a23e935111b871da5b15938597d74873
GergoKovacs0 <c9kovger@vasvari.org>
2025-03-27 23:40:13 +0100

    Implement language selection in navigation with state management and country flags

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React, { useState } from "react";
+
+import languageChangeEvent from "../events/LanguageChangeEvent";
+
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+import { MdOutlineSimCardDownload } from "react-icons/md";
+import ReactCountryFlag from "react-country-flag";
+import { translationsNavigation } from "../content/Translations";
 
 const Navigation: React.FC = () => {
+  const [language, setLanguage] = useState<"hu" | "gb">("hu");
+  const [content, setContent] = useState(translationsNavigation["hu"]);
+
+  const handleLanguageChange = (newLanguage: "hu" | "gb") => {
+    setLanguage(newLanguage);
+    setContent(translationsNavigation[newLanguage]);
+    sessionStorage.setItem("language", newLanguage);
+    languageChangeEvent();
+  };
+
   return (
     <Navbar className="mb-5 p-2 ps-4" bg="light" variant="light">
-      <Navbar.Brand>Kovacs Gergo</Navbar.Brand>
-      <Container className="ms-auto">
-        <Nav className="me-auto">
-          <Nav.Link href="/">Projektek</Nav.Link>
-          <Nav.Link href="/contact">Kapcsolat</Nav.Link>
-        </Nav>
-      </Container>
+      <Navbar.Brand>{content.brand}</Navbar.Brand>
+      <Nav className="ms-auto">
+        <Nav.Link href="#projects">{content.projects}</Nav.Link>
+        <Nav.Link href="/contact">{content.contact}</Nav.Link>
+        <Nav.Link href={"/" + content.fileName} download="portfolio.pdf">
+          <MdOutlineSimCardDownload />
+          {content.portfolio}
+        </Nav.Link>
+        <NavDropdown
+          title={<ReactCountryFlag countryCode={language} svg />}
+          id="basic-nav-dropdown"
+          align="end"
+        >
+          <NavDropdown.Item onClick={() => handleLanguageChange("hu")}>
+            <ReactCountryFlag className="emojiFlag" countryCode="HU" svg />{" "}
+          </NavDropdown.Item>
+          <NavDropdown.Item onClick={() => handleLanguageChange("gb")}>
+            <ReactCountryFlag className="emojiFlag" countryCode="GB" svg />{" "}
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
     </Navbar>
   );
 };
